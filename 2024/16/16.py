c0b03dbd57092933710d2eecfef34e7971330c82
zarify <2507851+zarify@users.noreply.github.com>
2024-12-17 08:03:16 +0800

    day 16 part 2

--- 2024/16/16.py
+++ 2024/16/16.py
@@ -1,15 +1,15 @@
 from collections import deque
 
-with open("test") as f:
+with open("input") as f:
     data = f.read().strip().split("\n")
 
 m = {}
-# facings and valid directions after a single rotation
+# facings and available moves
 f = {
-    (0, 1): [(1, 0), (-1, 0)],
-    (0, -1): [(1, 0), (-1, 0)],
-    (1, 0): [(0, 1), (0, -1)],
-    (-1, 0): [(0, 1), (-1, 0)],
+    (0, 1): [(1, 0), (-1, 0), (0, 1)],
+    (0, -1): [(1, 0), (-1, 0), (0, -1)],
+    (1, 0): [(0, 1), (0, -1), (1, 0)],
+    (-1, 0): [(0, 1), (0, -1), (-1, 0)],
 }
 facings = {(0, 1): ">", (0, -1): "<", (1, 0): "v", (-1, 0): "^"}
 for r, row in enumerate(data):
@@ -21,57 +21,106 @@ for r, row in enumerate(data):
             end = (r, c)
 
 
-def traverse(m, pos, facing):
-    costs = {p: (-1, None) for p in m}
-    # traverse the map m from position pos with facing facing
-    # keeping track of seen nodes and updating costs
-    seen = set()
-    queue = [(pos, facing)]
-    costs[pos] = (0, facing)  # start position
-    while queue:
-        pos, facing = queue.pop()
-        current_cost, from_dir = costs[pos]
-        seen.add(pos)
-        for d in f:
-            nxt = (pos[0] + d[0], pos[1] + d[1])
-            if m[nxt] == "#":
+class Reindeer:
+    def __init__(self, pos, facing, seen, cost):
+        self.pos = pos
+        self.facing = facing
+        self.seen = seen
+        self.cost = cost
+        self.babies = []
+        self.exhausted = False
+
+    def move(self, target):
+        self.babies = []  # don't want to drag the kids around!
+        self.seen.add((self.pos, self.facing))
+        if self.pos == target:  # arrived!
+            self.exhausted = True
+            return True
+        # potential moves: [(pos, facing, cost), ...]
+        potentials = []
+        for d in f[self.facing]:
+            np = (self.pos[0] + d[0], self.pos[1] + d[1])
+            if m[np] == "#" or (np, d) in self.seen or np in self.path_points():
                 continue
-            if d == facing:
-                nxt_cost = current_cost + 1
+            if d == self.facing:
+                # check for positions we've moved before, not turn combos
+                if np in self.path_points():
+                    continue
+                potentials.append((np, d, self.cost + 1))
             else:
-                nxt_cost = current_cost + 1001
-            # check if the path to nxt is cheaper, even if it's
-            # in seen
-            if nxt_cost < costs[nxt][0] or costs[nxt][0] == -1:
-                costs[nxt] = (nxt_cost, (d[0] * -1, d[1] * -1))
-                if nxt in seen:
-                    seen.remove(nxt)  # let us reprocess using the cheaper cost
-            if nxt in seen:
-                continue
-            queue.append((nxt, d))
-    return costs
+                if (self.pos, d) in self.seen:  # made this turn before
+                    continue
+                potentials.append((self.pos, d, self.cost + 1000))
+
+        if len(potentials) == 0:  # this reindeer has exhausted all options
+            self.exhausted = True
+        else:
+            # update this reindeer according to the first valid move/turn
+            self.pos = potentials[0][0]
+            self.facing = potentials[0][1]
+            self.cost = potentials[0][2]
+            # add clones for the other valid moves
+            for p in potentials[1:]:
+                self.babies.append(
+                    Reindeer(
+                        p[0],  # position
+                        p[1],  # facing
+                        self.seen.copy(),  # copy of seen positions and facings
+                        p[2],  # cost
+                    )
+                )
+
+        return False  # not arrived
+
+    def path_points(self):
+        return [p[0] for p in self.seen]
+
+    def get_babies(self):
+        return self.babies
+
+    def can_move(self):
+        return not self.exhausted
+
+    def get_cost(self):
+        return self.cost
+
+    def __repr__(self):
+        return f"{self.pos} {facings[self.facing]} cost {self.cost}"
 
 
-def backtrack(costs, end):
-    # backtrack from the end position, following the minimum cost route
-    # until we reach pos (the start)
-    # return the total number of positions traversed through
-    seen = set()
-    queue = [end]
-    while queue:
-        pos = queue.pop()
-        seen.add(pos)
-        cost, facing = costs[pos]
-        nxt = [(pos[0] + d[0], pos[1] + d[1]) for d in f]
-        # filter for visited positions and anything with no calculated cost
-        nxt = [n for n in nxt if n not in seen and costs[n][1]]
-        # any unseen neighbours that have the same minimum cost get
-        # added to the processing queue
-        for n in nxt:
-            if costs[n][0] == costs[min(nxt, key=lambda x: costs[x][0])]:
-                queue.append(n)
+def traverse(herd, end):
+    winners = []
+    global_costs = {}  # maintain global costs and use to proactively prune
+    while herd:
+        new_reindeer = []
+        for reindeer in herd:
+            # move the reindeer and check if it has arrived at end
+            arrived = reindeer.move(end)
+            # add any reindeer who have arrived to the winners list
+            if arrived:
+                winners.append(reindeer)
+            # collect any new reindeer from forks in the path
+            new_reindeer.extend(reindeer.get_babies())
+        # prune tired reindeer from the herd and add any babies
+        herd.extend(new_reindeer)
 
-    return len(seen)
+        for r in herd:
+            if r.cost < global_costs.get((r.pos, r.facing), float("inf")):
+                global_costs[(r.pos, r.facing)] = r.cost
+            # exhaust reindeer whoses costs are higher than the current best
+            # map position/facing cost
+            if r.cost > global_costs.get((r.pos, r.facing), float("inf")):
+                r.exhausted = True
+
+        # exhaust reindeer, winners, and those whose costs are too high
+        herd = [r for r in herd if r.can_move()]
+
+        if winners:  # prune reindeer who have costs greater than lowest winner
+            lowest = min([w.cost for w in winners])
+            herd = [r for r in herd if r.cost <= lowest]
+        # input("...")
+
+    return winners
 
 
 def vis(path, m):
@@ -85,13 +134,14 @@ def vis(path, m):
         print()
 
 
-# Part A
-costs = traverse(m, pos, (0, 1))
-print(f"Part A: {costs[end][0]}")
+rudolf = Reindeer(pos, (0, 1), set([pos]), 0)
 
-checking = [(7, 5), (7, 4), (7, 3), (8, 5)]
-for c in checking:
-    print(f"Cost of {c} is {costs[c]}")
+# Part A
+winners = traverse([rudolf], end)
+min_cost = min([r.cost for r in winners])
+print(f"Part A: {min_cost}")
 
 # Part B
-print(f"Part B: {backtrack(costs, end)}")
+winners = [w for w in winners if w.cost == min_cost]
+path_points = set().union(*[r.path_points() for r in winners])
+print(f"Part B: {len(path_points)-1}")
