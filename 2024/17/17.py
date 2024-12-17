6eecb497ee126910c637c4d9f4f8e9e0ed26f84e
zarify <2507851+zarify@users.noreply.github.com>
2024-12-17 15:45:01 +0800

    day 17 part 2 (had to look for hints for p2)

--- 2024/17/17.py
+++ 2024/17/17.py
@@ -1,4 +1,5 @@
 from math import pow
+from collections import deque
 
 with open("input") as f:
     data = f.read().strip().split("\n\n")
@@ -7,8 +8,6 @@ registers = {"ABC"[i]: int(v.split()[2]) for i, v in enumerate(data[0].split("\n
 program = list(map(int, data[1].split()[1].split(",")))
 
 registers["IP"] = 0
-
-output = []
 
 
 def combo(n):
@@ -58,19 +57,46 @@ def bxc(op):
 
 def out(op):
     op = combo(op) % 8
-    output.append(op)
     registers["IP"] += 2
+    return op
 
 
+#             0    1    2    3    4    5    6    7
 commands = [adv, bxl, bst, jnz, bxc, out, bdv, cdv]
 
+
+def run():
+    output = []
+    while registers["IP"] < len(program):
+        cmd = program[registers["IP"]]
+        op = program[registers["IP"] + 1]
+        v = commands[cmd](op)
+        if v is not None:
+            output.append(v)
+    return output
+
+
 # Part A
-while registers["IP"] < len(program):
-    cmd = program[registers["IP"]]
-    op = program[registers["IP"] + 1]
-    # print(cmd, op)
-    commands[cmd](op)
-    # print(registers, output)
-print(",".join(map(str, output)))
+print(",".join(map(str, run())))
 
 # Part B
+# had to read up on some of the approaches on reddit for this, doubt I would have
+# got it by myself
+partials = deque(
+    [(0, len(program) - 1)]
+)  # BFS - DFS finds a higher soln before the lower one
+found = False
+while partials and not found:
+    A, pos = partials.popleft()
+    for i in range(8):
+        registers = {"A": A * 8 + i, "B": 0, "C": 0, "IP": 0}
+        o = run()
+        if o == program[pos:]:  # how much of the solution are we checking?
+            if pos == 0:  # gotten to the beginning and a complete match
+                A = A * 8 + i
+                found = True
+                break
+            partials.append((A * 8 + i, pos - 1))
+
+
+print(f"Part B: {A}")
