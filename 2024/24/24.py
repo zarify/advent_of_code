9b7d1a0085c7339face0e8b596fff5ce4ebcf732
zarify <rob@headtilt.me>
2024-12-26 07:24:01 +0800

    day 24 part 2

--- 2024/24/24.py
+++ 2024/24/24.py
@@ -1,6 +1,3 @@
-from itertools import permutations, combinations
-from copy import deepcopy
-
 with open("input") as f:
     data = f.read().strip().split("\n\n")
 
@@ -61,25 +58,6 @@ def run(gates):
     return pref_wires, out
 
 
-def shuffle(gates, n):
-    index_combos = combinations(range(len(gates)), n)
-
-    unique_pairs = []
-    for combo in index_combos:
-        pairs = list(combinations(combo, 2))
-        if len(pairs) == n // 2:
-            unique_pairs.append(pairs)
-
-    for pairs in unique_pairs:
-        for perm in permutations(pairs):
-            dcopy = deepcopy(gates)
-            swaps = []
-            for a, b in perm:
-                swaps.extend([dcopy[a]["out"], dcopy[b]["out"]])
-                dcopy[a]["in"], dcopy[b]["in"] = dcopy[b]["in"], dcopy[a]["in"]
-            yield swaps, dcopy
-
-
 # Part A
 pref_wires, out = run(gates)
 answer = int("".join(str(w) for w in out), 2)
@@ -87,38 +65,26 @@ print(f"Part A: {answer}")
 
 
 # Part B
-def verify(x, y, z):
-    return x & y == z
+# adder rules: - https://www.reddit.com/r/adventofcode/comments/1hla5ql/comment/m3kws15/
+# - output gate: must be ^ unless it's the last bit (z45)
+# - not x,y inputs and not z output: must be &, |, not ^
+# - look one ahead:
+#   - a ^ with xy inputs must be ^, otherwise the input gate is faulty
+#   - an & must be an | with this as an input, else the & gate is fault
 
-
-x_b = [wires[w] for w in pref_wires["x"]]
-y_b = [wires[w] for w in pref_wires["y"]]
-x_answer = int("".join([str(wires[w]) for w in pref_wires["x"]]), 2)
-y_answer = int("".join([str(wires[w]) for w in pref_wires["y"]]), 2)
-print(f"X: {[0] * (len(out) - len(x_b)) + x_b}")
-print(f"Y: {[0] * (len(out) - len(y_b)) + y_b}")
-print(f"Z: {out}")
-print(x_answer, y_answer, answer, x_answer + y_answer)
-
-# testing test case 3 - YES, this operates as I expect it should
-# dc = deepcopy(gates)
-# dc[0]["in"], dc[5]["in"] = dc[5]["in"], dc[0]["in"]
-# dc[1]["in"], dc[2]["in"] = dc[2]["in"], dc[1]["in"]
-# _, out = run(dc)
-# answer = int("".join(str(w) for w in out), 2)
-# print(f"x {x_answer} y {y_answer} z {answer} {verify(x_answer, y_answer, answer)}")
-
-# WHAT IF
-# we try a different assumption - we pick a bit in the calculate 'z' that is incorrect
-# run a backward sim and try flipping the gates for two of the inputs and recalc, so
-# rather than trying all the combos, limit ourselves to the gates that are actually giving
-# bad results
-
-# combos = shuffle(gates, 4)  # test3 is 2 pairs [4], real is 4 [8]
-# for swaps, gate_copy in combos:
-#     _, out = run(gate_copy)
-#     answer = int("".join(str(w) for w in out), 2)
-#     if verify(x_answer, y_answer, answer):
-#         print(f"Found it {swaps}")
-# print(f"x: {x_answer} y: {y_answer}, z: {answer}")
-# should be (3, 5) and (0, 1)
+out_wires = set()
+for i, g in enumerate(gates):
+    if g["out"][0] == "z" and g["op"] != "XOR" and g["out"] != "z45":
+        out_wires.add(g["out"])
+    if g["in"][0][0] not in "xy" and g["in"][1][0] not in "xy" and g["out"][0] != "z":
+        if g["op"] == "XOR":
+            out_wires.add(g["out"])
+    if g["op"] == "XOR" and g["in"][0][0] in "xy" and g["in"][1][0] in "xy":
+        sus = [ga for ga in gates if g["out"] in ga["in"] and ga["op"] == "XOR"]
+        if sus == [] and g["in"] != ("x00", "y00"):
+            out_wires.add(g["out"])
+    if g["op"] == "AND":
+        sus = [ga for ga in gates if g["out"] in ga["in"] and ga["op"] == "OR"]
+        if sus == [] and g["in"] != ("x00", "y00"):
+            out_wires.add(g["out"])
+print(f"Part B: {','.join(sorted(out_wires))}")
